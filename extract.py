c2b103f345227550ffba560f614d815849d42d6c
Siva Kumar Bokam <107838342+Sivakumar3988@users.noreply.github.com>
2025-05-05 12:51:48 +0530

    updated with comments

--- extract.py
+++ extract.py
@@ -1,18 +1,22 @@
+#Importing the required libraries
 import re
 import nltk
 from nltk.corpus import stopwords
 from skills_list import skills
 
-
+# Download the list of stopwords from the NLTK library
 nltk.download('stopwords')
+# Importing the list of English stopwords into a Python
 stop_words = set(stopwords.words('english'))
 
+# Function to clean input text by lowercasing, removing extra spaces, and punctuation
 def clean_text(text: str) -> str:
     text = text.lower()
     text = re.sub(r'\s+', ' ',text)
     text = re.sub(r'[^\w\s]','',text)
     return text
 
+# Function to extract matching skills from the cleaned text
 def extract_skills(text: str) -> list:
     text = clean_text(text)
     found = set()
